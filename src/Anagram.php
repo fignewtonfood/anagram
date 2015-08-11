c201ef65b9c97bf99cc358328652be1214b89078
Kyle Pratuch & Tim White <student@epicodus.com>
2015-08-11 12:35:30 -0700

    add to method to pass fifth spec, incomplete

--- src/Anagram.php
+++ src/Anagram.php
@@ -4,19 +4,21 @@
     {
         function compareWords($input_words)
         {
-            $first_word = $input_words[0];
-            // echo $first_word;
-            $second_word = $input_words[1];
-            // echo $second_word;
-            $first_array = str_split($first_word);
-            // print_r($first_array);
-            $second_array = str_split($second_word);
-            // print_r($second_array);
-            sort($first_array);
-            // print_r($first_sort);
-            sort($second_array);
-            // print_r($second_sort);
-            $output_array = array();
+            $array_of_words = array();
+
+            $split_array =  array();
+
+            foreach ($input_words as $input) {
+                $split_array = str_split($input);
+                sort($split_array);
+                array_push($array_of_words, $split_array);
+            }
+
+            foreach ($array_of_words as $ordered_word) {
+                if ($ordered_word=>[] != 0) {
+                    if ($words[0] == $words)
+                }
+            }
 
             if ($first_array == $second_array) {
                 array_push ($output_array, true);
